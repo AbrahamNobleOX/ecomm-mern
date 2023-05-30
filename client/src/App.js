6f89e19662a7ec50314c916efe0077d111f4db1e
Akintola Abraham <akintolaabrahamidowu@gmail.com>
2023-05-30 15:22:06 +0100

    initial commit re-arranged

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Menu from "./components/nav/Menu";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Hello World!</h1>
+    </div>
   );
 }
