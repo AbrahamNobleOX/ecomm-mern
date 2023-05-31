ba4e4614f74c3fc945ddda08ec6aa80daab5ea60
Akintola Abraham <abramseyakid@gmail.com>
2023-05-31 16:02:01 +0100

    Changed world to customers

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div className="container main-content mb-5">
-        <Jumbotron title="Hello World" />
+        <Jumbotron title="Hello Customers..." />
         <Cards
           caption="Some quick example text."
           image="https://th.bing.com/th/id/R.e1707c345d5ac10c80a674030e606643?rik=pOsTg5KBoLuNvw&riu=http%3a%2f%2fwww.snut.fr%2fwp-content%2fuploads%2f2015%2f08%2fimage-de-paysage.jpg&ehk=1O5SWKkGpZ8yU%2b%2fAnLXG1v8k6BKxgyiXgHbOWBW1ir0%3d&risl=1&pid=ImgRaw&r=0"
