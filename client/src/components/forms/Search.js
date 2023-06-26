63d6e89d683d08edac52d3a39e992d4030100bdf
Akintola Abraham <abramseyakid@gmail.com>
2023-06-26 05:57:37 +0100

    Added Menu search functionality

--- client/src/components/forms/Search.js
+++ client/src/components/forms/Search.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import axios from "axios";
 import { useSearch } from "../../context/search";
 
@@ -6,18 +7,28 @@ export default function Search() {
   const [values, setValues] = useSearch();
 
   // Handle form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
+  const searchProducts = async (e) => {
     try {
       // Send an HTTP GET request to the server to search for products based on the keyword
       const { data } = await axios.get(`/products/search/${values?.keyword}`);
       // console.log(data);
 
       // Update the search context state with the received search results
+      // The values object is spread (...values) to retain the existing values
       setValues({ ...values, results: data });
     } catch (err) {
       console.log(err); // Log any errors that occur during the request
     }
+  };
+
+  // Use the useEffect hook to trigger the search when the keyword changes
+  useEffect(() => {
+    searchProducts();
+  }, [values.keyword]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    searchProducts(); // Trigger the search when the form is submitted
   };
 
   return (
@@ -27,21 +38,19 @@ export default function Search() {
           type="text"
           className="form-control"
           placeholder="Search..."
-          aria-label="Searc"
+          aria-label="Search"
           aria-describedby="basic-addon1"
           onChange={(e) => setValues({ ...values, keyword: e.target.value })}
           value={values.keyword}
         />
-        <span className="input-group-text" id="basic-addon1">
-          <i className="bi bi-search" />{" "}
-          <button
-            className="btn btn-outline-primary"
-            type="submit"
-            style={{ borderRadius: "0px" }}
-          >
-            Search {values.results.length}
-          </button>
-        </span>
+        <button
+          class="btn btn-outline-primary"
+          type="submit"
+          id="button-addon2"
+        >
+          <i className="bi bi-search mx-1" />
+          {values.results.length}
+        </button>
       </div>
     </form>
   );
